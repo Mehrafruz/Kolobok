18fe510d7e78ab30b532b43f98c5b4fe3e06aad7
Mehrafruz <leila_98_01@mail.ru>
2020-11-21 02:42:05 +0300

    partially implemented VIPER

--- WhereGoWithoutSB/Home/CategoryScreen/CategoryElements/Views/CategoryTableViewCell.swift
+++ WhereGoWithoutSB/Home/CategoryScreen/CategoryElements/Views/CategoryTableViewCell.swift
@@ -43,7 +43,7 @@ class CategoryTableViewCell: UITableViewCell {
         adressImageView.image = UIImage(named: "adress")
         timeTableImageView.image = UIImage(named: "timeTable")
         subwayImageView.image = UIImage(named: "subway")
-        titleLabel.font = UIFont(name: "CM_GARDEN_R", size: 22)
+        titleLabel.font = UIFont(name: "CM_GARDEN_R", size: 23)
         adressLabel.font = UIFont(name: "CM_GARDEN_R", size: 14)
         subwayLabel.font = UIFont(name: "CM_GARDEN_R", size: 14)
         timeLabel.font = UIFont(name: "CM_GARDEN_R", size: 14)
